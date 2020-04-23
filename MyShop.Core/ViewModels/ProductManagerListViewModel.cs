9e99ec7e3b05957b97c7e27ae9153b4a359d01e1
Patiparn <psanguanpao@gmail.com>
2020-04-23 22:53:45 +0700

    Implement ProductFilter

---
+++ MyShop.Core/ViewModels/ProductManagerListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyShop.Core.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductManagerListViewModel
+    {
+        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategory { get; set; }
+    }
+}
--- MyShopUI/Controllers/HomeController.cs
+++ MyShopUI/Controllers/HomeController.cs
@@ -1,30 +1,70 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using MyShop.DataAccess.SQL;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MyShopUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(SQLRepository<Product> productContext, SQLRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
 
-        public ActionResult About()
+        public ActionResult Index(string Category = null)
         {
-            ViewBag.Message = "Your application description page.";
+            //List<Product> products = context.Collection().ToList();
 
-            return View();
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(cat => cat.Category == Category).ToList();
+            }
+
+            ProductManagerListViewModel listViewModel = new ProductManagerListViewModel();
+            listViewModel.Product = products;
+            listViewModel.ProductCategory = categories;
+            return View(listViewModel);
         }
 
-        public ActionResult Contact()
+        public ActionResult Detail(string Id)
         {
-            ViewBag.Message = "Your contact page.";
+            Product productDetail = context.Find(Id);
+            if (productDetail == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return RedirectToAction("ProductDetail", new { id = Id });
+            }
+        }
 
-            return View();
+        public ActionResult ProductDetail(string Id)
+        {
+            Product productDetail = context.Find(Id);
+            if (productDetail == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productDetail);
+            }
         }
     }
 }
