67e11cfeef648a7d6ca6f6cd6d724214f3372029
Patiparn <psanguanpao@gmail.com>
2020-04-24 11:27:20 +0700

    Finished BasketService

---
+++ MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.Services
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpcontext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpcontext);
+        void RemoveFromBasket(HttpContextBase httpContext, string productId);
+    }
+}
